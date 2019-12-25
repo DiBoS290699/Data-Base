62ca64ec8299b27bd46c16040fc24a748f70facb
DiBoS290699 <dimaborisov290699@yandex.ru>
2019-12-25 10:41:34 +0400

    Add the control test and the one Add_task

---
+++ Other/Additional tasks/The trigger of the coherent add and remove.sql
@@ -0,0 +1,29 @@
+﻿-- Триггер, который при удалении блюда, удаляет связанные с ним записи,
+-- а при добавлении, вставляет запись в таблицу ингридиентов
+
+create or replace trigger communication
+    after delete or insert
+    on MENU
+    for each row
+declare
+    change_id number;
+    untitled  VARCHAR2(10) := 'untitled';
+begin
+    if INSERTING then
+        change_id := :new.ID;
+        insert into COMPOSITION values (change_id, untitled, 0, 'gram');
+    else
+        change_id := :old.ID;
+        delete from COMPOSITION where ID = change_id;
+    end if;
+end;
+
+delete
+from MENU
+where ID = 101;
+select *
+from COMPOSITION
+order by ID;
+
+insert into MENU
+values (101, 'borsch', 800, 1000, 1, 150, 10);
---
+++ Other/Control work/Task_1_from_Control_test.sql
@@ -0,0 +1,64 @@
+﻿create table N1_R1
+(
+    CUSTOMER VARCHAR2(50),
+    COUNTRY  VARCHAR2(50)
+);
+create table N1_R2
+(
+    SUPPLIER VARCHAR2(50),
+    CUSTOMER VARCHAR2(50),
+    N_ORDER  NUMBER(3),
+    PRICE    NUMBER(10)
+);
+create table N1_R3
+(
+    N_ORDER NUMBER(3),
+    PRODUCT VARCHAR2(50),
+    NUMB    NUMBER(10)
+);
+
+-----------------------------------------------------------------------------------
+--1
+select product
+from (select product, count(product) as c
+      from N1_R2
+               join N1_R3 N on N1_R2.N_ORDER = N.N_ORDER
+      group by product
+     )
+where c = 1;
+
+
+--2
+select SUPPLIER
+from N1_R2
+         join N1_R3 N on N1_R2.N_ORDER = N.N_ORDER
+group by SUPPLIER
+having sum(numb) = (
+    select max(s)
+    from (
+             select SUPPLIER, sum(numb) as s
+             from N1_R2
+                      join N1_R3 N on N1_R2.N_ORDER = N.N_ORDER
+             group by SUPPLIER));
+
+--3
+select SUPPLIER
+from N1_R2
+where SUPPLIER not in (select SUPPLIER
+                       from N1_R2
+                                join N1_R3 using (N_ORDER)
+                       where product in (select product
+                                         from N1_R3
+                                                  join N1_R2 using (N_ORDER)
+                                         where SUPPLIER = &sup
+                                         group by product)
+                       group by SUPPLIER)
+group by SUPPLIER;
+
+--4
+select N.CUSTOMER
+from N1_R2 join N1_R1 N on N1_R2.CUSTOMER = N.CUSTOMER
+    join (select COUNTRY, AVG(price) as avgp
+from N1_R1 join N1_R2 N on N1_R1.CUSTOMER = N.CUSTOMER
+group by COUNTRY) n3 on N.COUNTRY = n3.COUNTRY
+    where PRICE > avgp;
